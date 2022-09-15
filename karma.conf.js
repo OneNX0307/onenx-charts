7fcf0bf4884674d0fb6cbb911d340d229f7aba2f
alfredxinglkey <xingyafei0801@outlook.com>
2022-09-15 16:11:22 +0800

    feat: change module name.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/wafer-chart'),
+      dir: require('path').join(__dirname, './coverage/onenx-charts'),
       subdir: '.',
       reporters: [
         { type: 'html' },
